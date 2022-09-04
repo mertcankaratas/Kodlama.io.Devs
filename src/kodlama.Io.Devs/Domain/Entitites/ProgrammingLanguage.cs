ea8d3a382b0a2497950e5cd03736e53168f94fa2
Mertcan Karataş <can.mert.karatas@gmail.com>
2022-09-04 14:16:57 +0300

    Add project files.

---
+++ src/kodlama.Io.Devs/Domain/Entitites/ProgrammingLanguage.cs
@@ -0,0 +1,27 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entitites
+{
+    public class ProgrammingLanguage:Entity
+    {
+
+        public string Name { get; set; }
+        public string Version { get; set; }
+
+        public ProgrammingLanguage()
+        {
+
+        }
+        public ProgrammingLanguage(int id, string name, string version):this()
+        {
+            Id = id;
+            Name = name;
+            Version = version;
+        }
+    }
+}
